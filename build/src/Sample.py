e4a4525f58dbab5abbe1645b4433a7382c2ae836
anthonymesa <admesa@pm.me>
2023-03-29 07:28:29 -0400

    Update Sample.py

--- build/src/Sample.py
+++ build/src/Sample.py
@@ -4,6 +4,6 @@ from Release import ReleaseHandler
 import random
 
 class SampleHandler(ReleaseHandler):
-	def caculateReleaseScore(self, data):
+	def calculateReleaseScore(self, data):
 		score = random.random()
 		return score
